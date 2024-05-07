4f43748da0f59b281c8dce9b33b93d081f178507
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-05-07 12:39:00 -0700

    Binary search in rust syntax

--- Notes.md
+++ Notes.md
@@ -1,4 +1,6 @@
-# Exmple Notes
+# Notes
+
+## Run examples
 
 These examples are not directly related to the algorithms. But they are related to the book. It could have been an excersize or some side note to check.
 
@@ -8,8 +10,15 @@ To run the examples use:
 cargo run --release --example BASE_FILE_NAME ARGUMENTS
 ```
 
-## What's new
+## Rust what's new
 
 - [Simpler lint rules](https://blog.rust-lang.org/2023/11/16/Rust-1.74.0.html)
 - [Add Send bound to async result](https://blog.rust-lang.org/2023/12/21/async-fn-rpit-in-traits.html)
 - [Async recursion](https://blog.rust-lang.org/2024/03/21/Rust-1.77.0.html)
+
+## VS Integration
+
+- `Ctrl+Alt` shows the actual type
+- `Ctrl+J` opens the console
+- `main` has run icon
+- `Ctrl+I` Copilot modification
---
+++ examples/ch1-binary-search.rs
@@ -0,0 +1,48 @@
+use std::cmp::Ordering::*;
+
+fn main() {
+    let array: [i32; 5] = [1, 2, 3, 4, 5];
+    dbg!(array);
+
+    let searched = 4;
+    dbg!(searched);
+
+    let index = binary_search(&array, searched);
+    dbg!(index);
+}
+
+/// Performs a binary search on a sorted array to find the index of a given key.
+///
+/// # Arguments
+///
+/// * `a` - A reference to a sorted array of integers.
+/// * `kesearchedy` - The integer value to search for in the array.
+///
+/// # Returns
+///
+/// The index of the `key` in the array if found, otherwise -1.
+fn binary_search(array: &[i32; 5], searched: i32) -> i32 {
+    let mut lo = 0;
+    let mut hi = array.len() - 1;
+
+    while lo <= hi {
+        // Key is in a[lo..hi] or not present
+        // If lo == hi, then mid == lo = hi
+        let mid = lo + (hi - lo) / 2;
+
+        match Ord::cmp(&searched, &array[mid]) {
+            Less => {
+                hi = mid - 1;
+            }
+            Equal => {
+                return mid as i32;
+            }
+            Greater => {
+                lo = mid + 1;
+            }
+        }
+    }
+
+    // Key not found, li and hi did cross
+    -1
+}
