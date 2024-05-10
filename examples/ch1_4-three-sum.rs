716a5e8791a9d4704630e836d0e07a3acdf11981
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-05-10 13:59:56 -0700

    Future notes added

--- examples/ch1_4-three-sum.rs
+++ examples/ch1_4-three-sum.rs
@@ -9,7 +9,7 @@
 // http https://algs4.cs.princeton.edu/14analysis/32Kints.txt > 32Kints.txt
 // http https://algs4.cs.princeton.edu/14analysis/1Mints.txt > 1Mints.txt
 
-use std::{io::{BufRead, BufReader}};
+use std::{fs::File, io::{BufRead, BufReader}};
 use clap::{arg, Command};
 use anyhow::{anyhow, Ok, Result};
 
@@ -20,6 +20,27 @@ pub struct Config {
 
 /// cargo run --release --example ch1_4-three-sum examples\data\ch1_4-three-sum\1Kints.txt
 /// hyperfine.exe "cargo run --release --example ch1_4-three-sum examples\data\ch1_4-three-sum\1Kints.txt"
+pub fn run(config: Config) -> Result<()> {
+    dbg!(&config);
+
+    let reader = open(&config.in_file)?;
+    let mut numbers = Vec::new();
+
+    // TODO: Write terser code, figure out error propogation
+    for line in reader.lines() {
+        let line = line?;
+        let number = line.trim().parse::<i32>()?;
+        numbers.push(number);
+    }
+
+    dbg!(&numbers);
+
+    // TODO: the actual tree sum logic here
+    // TODO: measure performance on different input sizes
+
+    Ok(())
+}
+
 fn main() {
     if let Err(error) = get_args().and_then(run) {
         eprintln!("{error}");
@@ -31,9 +52,9 @@ pub fn get_args() -> Result<Config> {
     let mut matches = Command::new("three-sum")
         .version("1.0")
         .author("FallenGameR")
-        .about("Counts number of triples that sum to 0 in an array of integers")
+        .about("Counts number of triples that sum to 0 in an array of signed integers")
         .args([
-            arg!([INPUT_FILE] "Input file to process, stdin is -").default_value("-"),
+            arg!([INPUT_FILE] "Input file with signed integers to process, stdin is -").default_value("-"),
         ])
         .get_matches();
 
@@ -42,28 +63,9 @@ pub fn get_args() -> Result<Config> {
     })
 }
 
-pub fn run(config: Config) -> Result<()> {
-    dbg!(&config);
-
-    let reader = open(&config.in_file)?;
-    let mut numbers = Vec::new();
-
-    for line in reader.lines() {
-        let line = line?;
-        let number = line.trim().parse::<i32>()?;
-        numbers.push(number);
-    }
-
-    dbg!(&numbers);
-
-    Ok(())
-}
-
 fn open(path: &str) -> Result<Box<dyn BufRead>> {
     match path {
         "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
-        _ => Ok(Box::new(BufReader::new(
-            std::fs::File::open(path).map_err(|e| anyhow!("{path}: {e}"))?,
-        ))),
+        _ => Ok(Box::new(BufReader::new(File::open(path).map_err(|e| anyhow!("{path}: {e}"))?))),
     }
 }
