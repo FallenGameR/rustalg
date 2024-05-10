8937b8c0b5a7f129965b5ccc5a37dd52140e3945
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-05-10 13:26:59 -0700

    input data

---
+++ examples/ch1.4-three-sum.rs
@@ -0,0 +1,10 @@
+// https://algs4.cs.princeton.edu/14analysis/
+//
+// cd V:\src\rust\rustalg\examples\data\ch1.4-three-sum\
+// http https://algs4.cs.princeton.edu/14analysis/1Kints.txt > 1Kints.txt
+// http https://algs4.cs.princeton.edu/14analysis/2Kints.txt > 2Kints.txt
+// http https://algs4.cs.princeton.edu/14analysis/4Kints.txt > 4Kints.txt
+// http https://algs4.cs.princeton.edu/14analysis/8Kints.txt > 8Kints.txt
+// http https://algs4.cs.princeton.edu/14analysis/16Kints.txt > 16Kints.txt
+// http https://algs4.cs.princeton.edu/14analysis/32Kints.txt > 32Kints.txt
+// http https://algs4.cs.princeton.edu/14analysis/1Mints.txt > 1Mints.txt
