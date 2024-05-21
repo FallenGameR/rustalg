fbe17ad1b70eb5ed82156cd1c0e979967f54c1f0
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-05-21 12:57:19 -0700

    more notes

--- examples/ch1_p173-three-sum.rs
+++ examples/ch1_p173-three-sum.rs
@@ -9,7 +9,7 @@
 // http https://algs4.cs.princeton.edu/14analysis/32Kints.txt > 32Kints.txt
 // http https://algs4.cs.princeton.edu/14analysis/1Mints.txt > 1Mints.txt
 
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{fs::File, io::{BufRead, BufReader}, sync::{atomic::{AtomicU32, Ordering}, Arc}};
 use clap::{arg, Command};
 use anyhow::{anyhow, Ok, Result};
 use rayon::prelude::*;
@@ -45,14 +45,28 @@ x2 - x8
 
 this is O(n^3) algorithm
 
+cargo run --release --example ch1_p173-three-sum examples\data\rand\1K_int.txt
 cargo build --release --example ch1_p173-three-sum
 hyperfine.exe ".\target\release\examples\ch1_p173-three-sum.exe .\examples\data\rand\8K_int.txt"
 
 for_impl - 7s
 ranges_impl - 38s
-tuples_impl - 7s
+tuples_impl - 7s - same as for_impl, good cache utilization
+tuples_vec_par_filter_impl - out of memory if we collect all indexes
+partitions_impl - 18s - perf hit due to worse cache utilization
+partitions_par_impl - 2s - parralel version of partitions_impl
+ranges_arc_impl - 38s - same as ranges_impl, but with atomic counter, for_each is slow for some reason
+atomic_par_impl - 6s - parralel version of ranges_arc_impl, worse then partitions_par_impl since it uses a shared memory variable, bad for caches
+atomic_par_par_impl - 6s - rayon is smart about parralelism
+atomic_par_par_par_impl - 14s - lots of everhead due to parralelism
+atomic_par_trailing_impl - >1min - very inefficient to make small operations parralel
 
+https://smallcultfollowing.com/babysteps/blog/2015/12/18/rayon-data-parallelism-in-rust/
+https://github.com/rayon-rs/rayon/blob/main/README.md
+https://github.com/rayon-rs/rayon/blob/main/FAQ.md
+https://docs.rs/rayon/latest/rayon/
 */
+
 pub fn run(config: Config) -> Result<()> {
 
     // Parse numbers from file
@@ -66,12 +80,132 @@ pub fn run(config: Config) -> Result<()> {
 
     // Find three sum combinations that are zero
     // for_impl, ranges_impl, tuples_impl
-    let result = for_impl(numbers);
+    // tuples_vec_par_filter_impl
+    // partitions_impl, partitions_par_impl
+    // ranges_arc_impl, atomic_par_impl
+    let result = atomic_par_trailing_impl(numbers);
 
     // Print the output
     println!("{}", result);
 
     Ok(())
+}
+
+fn atomic_par_trailing_impl(numbers: Vec<i32>) -> usize {
+    let n = numbers.len();
+    let result = Arc::new(AtomicU32::new(0));
+
+    (0..n).for_each(|a|
+        (a+1..n).for_each(|b|
+            (b+1..n).into_par_iter().for_each(|c|
+                if numbers[a] + numbers[b] + numbers[c] == 0 {
+                    result.fetch_add(1, Ordering::Relaxed);
+                }
+            )
+        )
+    );
+
+    result.load(Ordering::Relaxed) as usize
+}
+
+
+fn atomic_par_par_par_impl(numbers: Vec<i32>) -> usize {
+    let n = numbers.len();
+    let result = Arc::new(AtomicU32::new(0));
+
+    (0..n).into_par_iter().for_each(|a|
+        (a+1..n).into_par_iter().for_each(|b|
+            (b+1..n).into_par_iter().for_each(|c|
+                if numbers[a] + numbers[b] + numbers[c] == 0 {
+                    //let counter = Arc::clone(&counter);
+                    result.fetch_add(1, Ordering::Relaxed);
+                }
+            )
+        )
+    );
+
+    result.load(Ordering::Relaxed) as usize
+}
+
+fn atomic_par_par_impl(numbers: Vec<i32>) -> usize {
+    let n = numbers.len();
+    let result = Arc::new(AtomicU32::new(0));
+
+    (0..n).into_par_iter().for_each(|a|
+        (a+1..n).into_par_iter().for_each(|b|
+            (b+1..n).for_each(|c|
+                if numbers[a] + numbers[b] + numbers[c] == 0 {
+                    result.fetch_add(1, Ordering::Relaxed);
+                }
+            )
+        )
+    );
+
+    result.load(Ordering::Relaxed) as usize
+}
+
+fn atomic_par_impl(numbers: Vec<i32>) -> usize {
+    let n = numbers.len();
+    let result = Arc::new(AtomicU32::new(0));
+
+    (0..n).into_par_iter().for_each(|a|
+        (a+1..n).for_each(|b|
+            (b+1..n).for_each(|c|
+                if numbers[a] + numbers[b] + numbers[c] == 0 {
+                    result.fetch_add(1, Ordering::Relaxed);
+                }
+            )
+        )
+    );
+
+    result.load(Ordering::Relaxed) as usize
+}
+
+fn ranges_arc_impl(numbers: Vec<i32>) -> usize {
+    let n = numbers.len();
+    let result = Arc::new(AtomicU32::new(0));
+
+    (0..n).for_each(|a|
+        (a+1..n).for_each(|b|
+            (b+1..n).for_each(|c|
+                if numbers[a] + numbers[b] + numbers[c] == 0 {
+                    result.fetch_add(1, Ordering::Relaxed);
+                }
+            )
+        )
+    );
+
+    result.load(Ordering::Relaxed) as usize
+}
+
+fn partitions_par_impl(numbers: Vec<i32>) -> usize {
+    let n = numbers.len();
+
+    let partitions =
+        (0..n).into_par_iter().map(move |a| -> usize {
+            (a+1..n).flat_map(move |b|
+                (b+1..n).map(move |c|
+                    (a,b,c)
+                )).filter(|(a,b,c)| numbers[*a] + numbers[*b] + numbers[*c] == 0 ).count()
+            }
+        );
+
+    partitions.sum()
+}
+
+fn partitions_impl(numbers: Vec<i32>) -> usize {
+    let n = numbers.len();
+
+    let partitions =
+        (0..n).map(move |a| -> usize {
+            (a+1..n).flat_map(move |b|
+                (b+1..n).map(move |c|
+                    (a,b,c)
+                )).filter(|(a,b,c)| numbers[*a] + numbers[*b] + numbers[*c] == 0 ).count()
+            }
+        );
+
+    partitions.sum()
 }
 
 fn tuples_impl(numbers: Vec<i32>) -> usize {
