bf805da97d6e9e7de24ae406bcdd7ce2de0c81a3
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2025-06-12 12:56:22 -0700

    Refactor dice simulation experiment documentation and improve RNG usage

--- examples/002_p061_dice-sumulation.rs
+++ examples/002_p061_dice-sumulation.rs
@@ -1,46 +1,32 @@
-// Experiment 1.1.35 Dice simulation.
-// Calculate the theoretical probability of each sum of two dices.
-// Compare with the random experiment.
-// How many dice throws are needed to get an agreement with precision better than 0.001?
-//
-// Answer: 100k-200k throws are needed
-
 use rand::Rng;
 
 const DICE: usize = 6;
 const THRESHOLD: f64 = 0.001;
 
-fn get_theoretical_frequency() -> Vec<usize> {
-    let mut frequency = vec![0; DICE * 2 + 1];
-    for first in 1..=DICE {
-        for second in 1..=DICE {
-            frequency[first + second] += 1;
-        }
-    }
-    frequency
-}
-
-fn get_probability(frequency: &Vec<usize>, n: usize) -> Vec<f64> {
-    let mut probability = vec![0.0; DICE * 2 + 1];
-    for i in 2..=DICE * 2 {
-        probability[i] = frequency[i] as f64 / n as f64;
-    }
-    probability
-}
-
+///
+/// # Experiment `1.1.35` - Dice simulation
+///
+/// Calculate the theoretical probability of each sum of
+/// two d6 `DICE` dices and compare with the a random experiment.
+///
+/// How many dice throws are needed to get an agreement
+/// with precision `THRESHOLD` better than 0.001?
+///
+/// Answer: 50k-400k throws are needed, depends on the random seed.
+///
 fn main() {
     let theoretical_frequency = get_theoretical_frequency();
     let theoretical_probability = get_probability(&theoretical_frequency, DICE * DICE);
     dbg!(&theoretical_probability);
 
-    let mut frequency = vec![0; DICE * 2 + 1];
-    let mut rng = rand::thread_rng();
+    let mut frequency = vec![0; DICE + DICE + 1];
+    let mut rng = rand::rng();
     let mut n = 0;
 
     loop {
         n += 1;
-        let first: usize = rng.gen_range(1..=6);
-        let second: usize = rng.gen_range(1..=6);
+        let first: usize = rng.random_range(1..=DICE);
+        let second: usize = rng.random_range(1..=DICE);
         frequency[first + second] += 1;
 
         let probability = frequency[first + second] as f64 / n as f64;
@@ -66,4 +52,28 @@ fn main() {
 
     let probabilities = get_probability(&frequency, n);
     dbg!(probabilities);
+}
+
+fn get_theoretical_frequency() -> Vec<usize> {
+    // Elements at 0,1 are unused, as dice sums start from 2
+    // That allows to use index as the sum of two dice we want to get the frequency for
+    let mut frequency = vec![0; DICE + DICE + 1];
+
+    for first in 1..=DICE {
+        for second in 1..=DICE {
+            frequency[first + second] += 1;
+        }
+    }
+
+    frequency
+}
+
+fn get_probability(frequency: &Vec<usize>, n: usize) -> Vec<f64> {
+    let mut probability = vec![0.0; DICE + DICE + 1];
+
+    for i in 2..=DICE * 2 {
+        probability[i] = frequency[i] as f64 / n as f64;
+    }
+
+    probability
 }
