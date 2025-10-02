3cdb26670b345f12b39f7b4142921e0ee724fcf3
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2025-10-02 13:03:33 -0700

    .

--- src/bin/sort.rs
+++ src/bin/sort.rs
@@ -1,25 +1,21 @@
 // page 245
 
-use anyhow::{Result, anyhow};
-use clap::{arg, Command, Parser};
+use anyhow::Result;
+use clap::Parser;
 
-use rustalg::sort::args::{open, sort_bin::*};
+use rustalg::sort::args::{open, sort_bin::Algorithm};
 
 /// Sorts a file or input stream with various different algorithms
 #[derive(Parser, Debug)]
 #[command(name = "sort", version, about = "Sort utility")]
 pub struct Config {
-    /// Input file with strings to sort on each line, stdin is - to the (positional parameter), this comment is added to help
-    #[arg(default_value = "-")]
+    /// Input file with strings to sort on each line; use - for stdin
+    #[arg(value_name="INPUT", default_value="-")]
     in_file: String,
 
+    /// Sorting algorithm to use
+    #[arg(short='a', long="algorithm", value_enum, default_value_t=Algorithm::Insertion)]
     algorithm: Algorithm,
-}
-
-impl Config {
-    pub fn new(in_file: String, algorithm: Algorithm) -> Self {
-        Self { in_file, algorithm }
-    }
 }
 
 
@@ -29,47 +25,15 @@ impl Config {
 // hyperfine.exe --warmup 1 ".\target\release\union-find.exe .\data\union-find\largeUF.txt" # 338ms for WeightedUnionFind
 // hyperfine.exe --warmup 1 ".\target\release\union-find.exe .\data\union-find\largeUF.txt" # 328ms for WeightedUnionFindWithPathCompression (incremental)
 fn main() {
-    if let Err(error) = get_args().and_then(run) {
+    let config = Config::parse();
+    if let Err(error) = run(config) {
         eprintln!("{error}");
         std::process::exit(1);
     }
 }
 
-fn get_args() -> Result<Config> {
-    let cli = Config::parse();
-    if cli.command.is_none() {
-        return Ok(());
-    }
-
-    let in_file = cli.input_file;
-    let mut matches = Command::new("sorting")
-        .version("1.0")
-        .author("FallenGameR")
-        .about("Sorts a file or input stream")
-        .args([
-            arg!([INPUT_FILE] "Input file with strings to sort on each line, stdin is -").default_value("-"),
-            arg!(-a --algorithm <ALGORITHM> "Sorting algorithm to use: selection, insertion")
-                .value_parser(clap::builder::EnumValueParser::<Algorithm>::new())
-                .default_value("insertion"),
-        ])
-        .get_matches();
-
-    let in_file = matches
-        .remove_one("INPUT_FILE")
-        .expect("Input file not provided");
-    let algorithm = matches
-        .remove_one("algorithm")
-        .expect("Algorithm not provided");
-
-    Ok(Config::new(in_file, algorithm))
-}
-
 fn run(config: Config) -> Result<()> {
-    let reader = open(&config.in_file)?;
-
-    println!("Latest alg is to be done, yay!");
-
-    dbg!(config);
-
+    let _reader = open(&config.in_file)?;
+    println!("Algorithm: {:?}", config.algorithm);
     Ok(())
 }
